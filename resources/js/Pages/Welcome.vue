c0514d5186dbc160dffff2e7fb3bbea1f115c925
michaelanthonyfeather <80717544+michaelanthonyfeather@users.noreply.github.com>
2024-06-05 11:01:16 +0100

    Laravel + Jetstream Installation

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    }
+});
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <div class="relative min-h-screen flex flex-col items-center selection:bg-[#FF2D20] selection:text-white mt-8">
+        <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
+            <header class="flex justify-content-around gap-2 pb-10 lg:grid-cols-3">
+                <div class="flex">
+                    <ApplicationLogo/>
+                </div>
+                <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
+                    <Link
+                        v-if="$page.props.auth.user"
+                        :href="route('dashboard')"
+                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                    >
+                        Dashboard
+                    </Link>
+
+                    <template v-else>
+                        <Link
+                            :href="route('login')"
+                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                        >
+                            Log in
+                        </Link>
+
+                        <Link
+                            v-if="canRegister"
+                            :href="route('register')"
+                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                        >
+                            Register
+                        </Link>
+                    </template>
+                </nav>
+            </header>
+
+            <main class="">
+
+            </main>
+        </div>
+    </div>
+</template>
